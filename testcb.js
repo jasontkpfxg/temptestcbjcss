5cb7d783a5614e2ea02d3c5c837dc6f121e3e26e
jasontkpfxg <132967316+jasontkpfxg@users.noreply.github.com>
2023-07-28 15:03:49 +0800

    Update testcb.js

--- testcb.js
+++ testcb.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Attempt to set SameSite cookies from a CORS request
     function setSameSiteCookiesFromCORS() {
-        fetch("https://v-circle.com/", {
+        fetch(".v-circle.com/", {
             method: 'GET',
             credentials: 'include', // Include cookies in the CORS request
             headers: {
