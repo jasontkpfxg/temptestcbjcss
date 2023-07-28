0ff5a499df58f9ecdb170af7478b1b4c75d2f6e3
jasontkpfxg <132967316+jasontkpfxg@users.noreply.github.com>
2023-07-28 16:29:00 +0800

    Update testcb.js

--- testcb.js
+++ testcb.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const token = urlParams.get("token");
     const timestamp = Date.now();
 
-    var url = "https://vera-ai-chatbot.v-circle.com/chat/embed?" + timestamp;
+    var url = "https://localhost:4200/embed?" + timestamp;
 
     if (agentID)
         url = url + "&agentID=" + encodeURIComponent(agentID);
@@ -40,36 +40,6 @@ document.addEventListener("DOMContentLoaded", function () {
     cssLink.rel = "stylesheet";
     cssLink.href = "https://jasontkpfxg.github.io/temptestcbjcss/testcb.css"; // Replace with the actual URL of your CSS file
     document.head.appendChild(cssLink);
-
-    // Attempt to set SameSite cookies from a CORS request
-    function setSameSiteCookiesFromCORS() {
-        fetch("https://vera-ai-chatbot.v-circle.com/", {
-            method: 'GET',
-            credentials: 'include', // Include cookies in the CORS request
-            headers: {
-                'Authorization': 'Bearer ' + token,
-            }
-        })
-        .then(response => {
-            // Check if the CORS request was successful and the 'Set-Cookie' header is present
-            if (response.ok && response.headers.has('Set-Cookie')) {
-                // Extract the cookies from the 'Set-Cookie' header
-                const cookies = response.headers.get('Set-Cookie').split(';');
-
-                // Set the cookies with SameSite=None and Secure attributes on your domain
-                cookies.forEach(cookie => {
-                    document.cookie = cookie.trim() + '; SameSite=None; Secure';
-                });
-            }
-        })
-        .catch(error => {
-            // Handle errors
-            console.error('Error making CORS request:', error);
-        });
-    }
-
-    // Call the function to attempt to set SameSite cookies from the CORS request
-    setSameSiteCookiesFromCORS();
 });
 
 window.onmessage = function (e) {
