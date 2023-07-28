7cd31beb7e8be9da407ab716abefac5d1a3da322
jasontkpfxg <132967316+jasontkpfxg@users.noreply.github.com>
2023-07-28 14:32:31 +0800

    Update testcb.js

--- testcb.js
+++ testcb.js
@@ -1,19 +1,3 @@
-document.cookie = "_gcl_au=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_1QLPNMKFSB=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_7L7DVB3ESW=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_X40F2R9XTK=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_C2LNYF88H0=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_B7TFWF5EFS=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_090XLNZZ3Q=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_B7F9XHPRC6=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_H6LDWLC82Q=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_gid=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_B1QX9SERGQ=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_0QYMYJCHSX=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "googtrans=.v-circle.com/; SameSite=None; Secure";
-document.cookie = "_ga_DVL1YRSJLN=.v-circle.com/; SameSite=None; Secure";
-
 document.addEventListener("DOMContentLoaded", function () {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -56,6 +40,36 @@ document.addEventListener("DOMContentLoaded", function () {
     cssLink.rel = "stylesheet";
     cssLink.href = "https://jasontkpfxg.github.io/temptestcbjcss/testcb.css"; // Replace with the actual URL of your CSS file
     document.head.appendChild(cssLink);
+
+    // Attempt to set SameSite cookies from a CORS request
+    function setSameSiteCookiesFromCORS() {
+        fetch('https://vera-ai-chatbot.v-circle.com/api/some-endpoint', {
+            method: 'GET',
+            credentials: 'include', // Include cookies in the CORS request
+            headers: {
+                'Authorization': 'Bearer your-access-token',
+            }
+        })
+        .then(response => {
+            // Check if the CORS request was successful and the 'Set-Cookie' header is present
+            if (response.ok && response.headers.has('Set-Cookie')) {
+                // Extract the cookies from the 'Set-Cookie' header
+                const cookies = response.headers.get('Set-Cookie').split(';');
+
+                // Set the cookies with SameSite=None and Secure attributes on your domain
+                cookies.forEach(cookie => {
+                    document.cookie = cookie.trim() + '; SameSite=None; Secure';
+                });
+            }
+        })
+        .catch(error => {
+            // Handle errors
+            console.error('Error making CORS request:', error);
+        });
+    }
+
+    // Call the function to attempt to set SameSite cookies from the CORS request
+    setSameSiteCookiesFromCORS();
 });
 
 window.onmessage = function (e) {
